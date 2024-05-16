6622bf6c16b8616750e1a36ecfdd5069ed1cfe96
Guhanxue <hg119@duke.edu>
2024-05-16 01:46:52 +0000

    update demo

--- train_singlegpu_demo.sh
+++ train_singlegpu_demo.sh
@@ -7,6 +7,12 @@ export CUDA_VISIBLE_DEVICES="5"
 arch="vit_b"  # Change this value as needed
 finetune_type="adapter"
 dataset_name="MRI-Prostate"  # Assuming you set this if it's dynamic
+targets='combine_all' # make it as binary segmentation 'multi_all' for multi cls segmentation
+# Construct train and validation image list paths
+img_folder="./datasets"  # Assuming this is the folder where images are stored
+train_img_list="${img_folder}/${dataset_name}/train_5shot.csv"
+val_img_list="${img_folder}/${dataset_name}/val_5shot.csv"
+
 
 # Construct the checkpoint directory argument
 dir_checkpoint="2D-SAM_${arch}_decoder_${finetune_type}_${dataset_name}_noprompt"
@@ -17,6 +23,10 @@ python SingleGPU_train_finetune_noprompt.py \
     -finetune_type "$finetune_type" \
     -arch "$arch" \
     -if_mask_decoder_adapter True \
+    -img_folder "$img_folder" \
+    -mask_folder "$img_folder" \
     -sam_ckpt "sam_vit_b_01ec64.pth" \
     -dataset_name "$dataset_name" \
-    -dir_checkpoint "$dir_checkpoint"
+    -dir_checkpoint "$dir_checkpoint" \
+    -train_img_list "$train_img_list" \
+    -val_img_list "$val_img_list"
